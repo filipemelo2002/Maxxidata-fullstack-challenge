1549dadf05047d5ea572dfa4e49942ae650735e4
Filipe Melo <filipemelo032@gmail.com>
2021-03-09 18:53:04 -0300

    [BACKEND] initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+
+const app = express()
+app.use(express.json())
+app.use("/", (request, response)=>{
+  return response.json({message: "API is running"})
+})
+
+app.listen(3333, ()=> console.log("SERVER STARTED ON 3333"))
