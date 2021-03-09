cd8229572c02ab69ef4ac0d58e33d86e23d4aab3
Filipe Melo <filipemelo032@gmail.com>
2021-03-09 19:31:51 -0300

    [BACKEND] Added ESLINT config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,9 @@
-import express from 'express'
+import express from 'express';
 
+const app = express();
+app.use(express.json());
+app.use('/', (request, response) => {
+  return response.json({ message: 'API is running' });
+});
 
-const app = express()
-app.use(express.json())
-app.use("/", (request, response)=>{
-  return response.json({message: "API is running"})
-})
-
-app.listen(3333, ()=> console.log("SERVER STARTED ON 3333"))
+app.listen(3333, () => console.log('SERVER STARTED ON 3333'));
