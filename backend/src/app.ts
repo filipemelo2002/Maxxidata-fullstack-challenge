da2d116504fd2bffe8f561d375e3dd6b2a2e0dc0
Filipe Melo <filipemelo032@gmail.com>
2021-04-06 23:58:42 -0300

    Added CORS config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 import './database';
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
 import AppError from './errors/AppError';
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
